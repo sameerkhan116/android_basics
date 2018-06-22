35f9e4501b91a3cffdf57df857e0185409b04c4d
Sameer Khan <sameer_khan@student.uml.edu>
2018-06-22 14:35:23 -0400

    Navigating to other pages and intents

--- app/src/main/java/com/example/android/myapplication/MainActivity.java
+++ app/src/main/java/com/example/android/myapplication/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.android.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.widget.ImageView;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,10 +17,17 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Log.d(TAG, "onCreate: Activity has started");
 
-        ImageView firstImage = (ImageView) findViewById(R.id.firstImage);
-
-        int imageResource = getResources().getIdentifier("@drawable/cyberpunk", null, this.getPackageName());
-        firstImage.setImageResource(imageResource);
+        Button btnNav1 = (Button) findViewById(R.id.btnSecondScreen);
+        btnNav1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d(TAG, "onClick: Click second screen button");
+                Intent intent = new Intent(MainActivity.this, SecondScreen.class);
+                startActivity(intent);
+            }
+        });
     }
+
+
 
 }
